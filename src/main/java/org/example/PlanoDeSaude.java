0d8fe9e40694c923e21dcd186118dee83b36f411
vigoncalves200713 <vigoncalves200713@gmail.com>
2026-09-10 14:26:58 -0300

    aula 10/09

--- src/main/java/org/example/PlanoDeSaude.java
+++ src/main/java/org/example/PlanoDeSaude.java
@@ -1,6 +1,16 @@
 
-
 package org.example;
+import java.util.Scanner;
 
-public class PlanoDeSaude {
+public class PlanoDeSaude
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe o tempo de contratação da empresa: ");
+        int tempoContrato = sc.nextInt();
+
+        if (tempoContrato >= 3){
+            System.out.println("adesão ao plano de saude disponivel!");
+        }
+    }
 }
